005898b5845a9605afaa5b8c011f9882671e44ff
StoneCX <chengxcv5@163.com>
2017-02-24 15:56:14 +0800

    add a label to main View

--- gitDemo/gitDemo/ViewController.swift
+++ gitDemo/gitDemo/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let alabel = UILabel(frame:CGRect(x: 50, y: 50, width: 100, height: 30))
+        alabel.text = "this is a label"
+        
+        self.view .addSubview(alabel)
     }
 
     override func didReceiveMemoryWarning() {
